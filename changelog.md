172eb70760af5076ca98dca35a7e7261752813cc
Mr. Fluffy <git@sixearsm>
2022-11-13 06:55:48 +0000

    fix fixed-package-name typo in flake-build-utils

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+0.0.5.11 2022-11-13
+===================
+- fix fixed-package-name typo in flake-build-utils
+
 0.0.5.10 2022-11-04
 ===================
 - fix package names
--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.6";
+    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.10";
 
-    base0t.url       = "github:sixears/base0t/r0.0.1.5";
-    base1.url        = "github:sixears/base1/r0.0.9.10";
-    tasty-plus.url   = "github:sixears/tasty-plus/r1.5.2.9";
+    base0t.url       = "github:sixears/base0t/r0.0.1.6";
+    base1.url        = "github:sixears/base1/r0.0.9.11";
+    tasty-plus.url   = "github:sixears/tasty-plus/r1.5.2.10";
   };
 
   outputs = { self, nixpkgs, build-utils
