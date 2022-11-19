4b465b26387960f0b1820a42809109959a6d61c9
Mr. Fluffy <git@sixearsm>
2022-11-19 06:03:27 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Prelude replacement, first-level local packages, incl. tests";
 
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.12";
+    nixpkgs.url      = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+    build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    base0t.url       = "github:sixears/base0t/r0.0.1.10";
+    base0t.url       = github:sixears/base0t/r0.0.1.10;
     base1.url        = github:sixears/base1/r0.0.9.25;
     tasty-plus.url   = github:sixears/tasty-plus/r1.5.2.17;
   };
@@ -17,5 +17,17 @@
         inherit base0t base1 tasty-plus;
       };
       ghc = p: p.ghc8107; # for tfmt
+      callPackage = { mkDerivation, lib, mapPkg, system }:
+        let
+          pkg = build-utils.lib.flake-def-pkg system;
+        in
+          mkDerivation {
+            pname = "base1t";
+            version = "0.0.5.27";
+            src = ./.;
+            libraryHaskellDepends = mapPkg [ base0t base1 tasty-plus ];
+            description = "Prelude replacement, first-level local packages, incl. tests";
+            license = lib.licenses.mit;
+          };
     };
 }
