63b88c3ef064ec882b59958c74d260e8918e68ea
Mr. Fluffy <git@sixearsm>
2022-11-20 06:53:50 +0000

    exited->1.0.4.22;monaderror-io->1.2.5.19;tfmt->0.2.7.24;number->1.1.2.14;has-callstack->1.0.1.18;base0->0.0.4.11;more-unicode->0.0.17.12;quasiquoting->1.0.1.31

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
     nixpkgs.url      = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    base0t.url       = github:sixears/base0t/r0.0.1.13;
-    base1.url        = github:sixears/base1/r0.0.9.32;
-    tasty-plus.url   = github:sixears/tasty-plus/r1.5.2.22;
+    base0t.url       = github:sixears/base0t/r0.0.1.14;
+    base1.url        = github:sixears/base1/r0.0.9.33;
+    tasty-plus.url   = github:sixears/tasty-plus/r1.5.2.23;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -23,7 +23,7 @@
         in
           mkDerivation {
             pname = "base1t";
-            version = "0.0.5.34";
+            version = "0.0.5.35";
             src = ./.;
             libraryHaskellDepends = mapPkg [ base0t base1 tasty-plus ];
             description = "Prelude replacement, first-level local packages, incl. tests";
